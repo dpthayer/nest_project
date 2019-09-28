e9f6df4ea4addb19ad7b64e23ccdb69c0f1a1f86
dpthayer <dpthayer@gmail.com>
2019-09-27 17:08:54 -0700

    Up To PATCHController

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -2,14 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
 
     private tasks:Task[] = [];
 
+    
+
     getAllTasks() : Task[]{
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterdto: GetTaskFilterDto) : Task[]{
+        const { status, search } = filterdto;
+        let tasks = this.getAllTasks();
+        if (status){
+            tasks = tasks.filter(task => task.status === status)
+        }
+        if (search) {
+            tasks = tasks.filter( task => task.title.includes(search) ||
+            task.description.includes(search),);
+        }
+
+        return tasks;
+    }
+
+
+    getTaskById(id:string):Task {
+        return this.tasks.find(task => task,id === id);
     }
 
     createTask(createTaskDto:CreateTaskDto) : Task {
@@ -22,6 +44,19 @@ export class TasksService {
         };
 
         this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id:string): void {
+        //console.log('delete id', id);
+        this.tasks = this.tasks.filter(task => task.id !== id);
+       // console.log('tasks', this.tasks);
+    }
+
+    updateTaskStatus(id: string, status :TaskStatus) : Task {
+        console.log('update service id', id);
+        const task = this.getTaskById(id);
+        task.status = status;
         return task;
     }
 }
